66d626dab72cd807a27fe0b941d75969e9371ee3
Javi Olano <javiolanojol@gmail.com>
2021-02-23 14:44:53 +0100

    commit xD

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <testComponent/>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import testComponent from '@/components/testComponent.vue';
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    testComponent
   }
 };
 </script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import testComponent from "@/components/testComponent.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -10,3 +11,14 @@ describe("HelloWorld.vue", () => {
     expect(wrapper.text()).toMatch(msg);
   });
 });
+
+describe('testComponent.vue', () => {
+  it('should be pass when you render slots', () => {
+    const title = '<span>Title</span>'
+    const wrapper = shallowMount(testComponent, {
+      slots: { title }
+    })
+    expect(wrapper.html()).toMatch(title)
+  })
+})
+
