a953ad6c5c1dd07e208346f3f2edbf24ddc49794
Javi Olano <javiolanojol@gmail.com>
2021-02-24 14:31:48 +0100

    2 nuevas rutas

---
+++ src/views/Componente.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Nueva vista</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Componente"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Componente2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <h1>Edgar me debes 5 minutos primer aviso</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Componente2"
+}
+</script>
+
+<style scoped>
+
+</style>
